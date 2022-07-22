58b2426f7fa0b8e212d2f7fc05570a0babf6b4d9
6six7sven <sushant.679p@gmail.com>
2022-07-22 21:10:47 +0530

    init firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 
+import 'rsuite/dist/rsuite.min.css';
+import './styles/main.scss'
+
+
 function App() {
   return (
     <div>
---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyCa-v2R2Pg98gRYKRU8FidJPzvTaYAI3X8",
+    authDomain: "chat-web-app-477ad.firebaseapp.com",
+    projectId: "chat-web-app-477ad",
+    storageBucket: "chat-web-app-477ad.appspot.com",
+    messagingSenderId: "354955417264", 
+    appId: "1:354955417264:web:8c64072323ee296635cd75"
+  
+}
+
+const app = firebase.initializeApp(config);
+
