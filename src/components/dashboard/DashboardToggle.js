383a184bb60948b9244ece6bd2c7f0e17df096e1
6six7sven <sushant.679p@gmail.com>
2022-07-23 17:30:57 +0530

    started building dashboard, added signout button and username display

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { Button, Drawer, Icon } from "rsuite";
+import React, { useCallback } from "react";
+import { Alert, Button, Drawer, Icon } from "rsuite";
 import { useMediaQuery, useModalState } from "../../misc/custom-hooks";
 import Dashboard from ".";
+import { auth } from "../../misc/firebase";
 
 
 const DashboardToggle = () => {
@@ -9,13 +10,20 @@ const DashboardToggle = () => {
     const { isOpen, close, open } = useModalState();
     const isMobile = useMediaQuery('(max-width: 992px');
 
+    const onSignOut = useCallback(() => {
+        auth.signOut();
+
+        Alert.info('Signed out', 4000);
+        close();
+    }, [close])
+
     return(
         <>
             <Button block color="blue" onClick={open}>
                 <Icon icon = "dashboard" /> Dashboard
             </Button>
             <Drawer full={isMobile} show={isOpen} onHide={close} placement = "left">
-                <Dashboard />
+                <Dashboard onSignOut={onSignOut}/>
 
             </Drawer>
         </>
