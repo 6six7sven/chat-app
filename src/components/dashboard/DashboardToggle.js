2895f3a6af1274ccc3cb5fb422a2c936d21118cc
6six7sven <sushant.679p@gmail.com>
2022-07-23 17:17:51 +0530

    useMediaQuery

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Button, Drawer, Icon } from "rsuite";
-import { useModalState } from "../../misc/custom-hooks";
+import { useMediaQuery, useModalState } from "../../misc/custom-hooks";
 import Dashboard from ".";
 
 
 const DashboardToggle = () => {
 
-    const { isOpen, close, open } = useModalState()
+    const { isOpen, close, open } = useModalState();
+    const isMobile = useMediaQuery('(max-width: 992px');
 
     return(
         <>
             <Button block color="blue" onClick={open}>
                 <Icon icon = "dashboard" /> Dashboard
             </Button>
-            <Drawer show={isOpen} onHide={close} placement = "left">
+            <Drawer full={isMobile} show={isOpen} onHide={close} placement = "left">
                 <Dashboard />
 
             </Drawer>
